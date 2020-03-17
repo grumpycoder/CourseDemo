3e82981dc8193d9563c2365faf1e0443133efa7e
Mark <mark.lawrence@c2cschools.com>
2020-03-16 20:23:09 -0500

    init migration

---
+++ CourseDemo.Data/Migrations/20200316221022_init.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseDemo.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CourseLevels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseLevelCode = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseLevels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseTypeCode = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    IsCore = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeliveryTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeliveryTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grades", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    GroupName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    CourseCode = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    BeginYear = table.Column<int>(nullable: true),
+                    EndYear = table.Column<int>(nullable: true),
+                    BeginSequence = table.Column<int>(nullable: true),
+                    EndSequence = table.Column<int>(nullable: true),
+                    CreditRecoveryAvailable = table.Column<bool>(nullable: true),
+                    CreditAdvancementAvailable = table.Column<bool>(nullable: true),
+                    CreditTypes = table.Column<string>(nullable: true),
+                    Tags = table.Column<string>(nullable: true),
+                    CreditUnits = table.Column<decimal>(nullable: true),
+                    LowGradeId = table.Column<int>(nullable: true),
+                    HighGradeId = table.Column<int>(nullable: true),
+                    CourseLevelId = table.Column<int>(nullable: true),
+                    CourseTypeId = table.Column<int>(nullable: true),
+                    SubjectAreaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Courses_CourseLevels_CourseLevelId",
+                        column: x => x.CourseLevelId,
+                        principalTable: "CourseLevels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Courses_CourseTypes_CourseTypeId",
+                        column: x => x.CourseTypeId,
+                        principalTable: "CourseTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Courses_Grades_HighGradeId",
+                        column: x => x.HighGradeId,
+                        principalTable: "Grades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Courses_Grades_LowGradeId",
+                        column: x => x.LowGradeId,
+                        principalTable: "Grades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_CourseLevelId",
+                table: "Courses",
+                column: "CourseLevelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_CourseTypeId",
+                table: "Courses",
+                column: "CourseTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_HighGradeId",
+                table: "Courses",
+                column: "HighGradeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_LowGradeId",
+                table: "Courses",
+                column: "LowGradeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "DeliveryTypes");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "CourseLevels");
+
+            migrationBuilder.DropTable(
+                name: "CourseTypes");
+
+            migrationBuilder.DropTable(
+                name: "Grades");
+        }
+    }
+}
