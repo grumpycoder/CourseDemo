8b0338eae4cb64be75cd1514516932d5e0943c01
Mark <mark.lawrence@c2cschools.com>
2020-03-18 15:52:33 -0500

    add assign program dto

---
+++ CourseDemo.Domain/Dtos/AssignProgramDto.cs
@@ -0,0 +1,9 @@
+namespace CourseDemo.Domain.Dtos
+{
+    public class AssignProgramDto
+    {
+        public int ProgramId { get; set; }
+        public int BeginYear { get; set; }
+        public int? EndYear { get; set; }
+    }
+}
