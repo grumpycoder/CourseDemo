d27f484c2721e05a358ccbbd8a35994af56d58ed
Mark <mark.lawrence@c2cschools.com>
2020-03-18 15:40:28 -0500

    update migration for ProgramAssignments

---
+++ CourseDemo.Data/Migrations/20200318132224_program-assignments.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseDemo.Data.Migrations
+{
+    public partial class programassignments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubjectAreaId",
+                schema: "Common",
+                table: "Courses");
+
+            migrationBuilder.CreateTable(
+                name: "ProgramCourses",
+                schema: "CareerTech",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProgramId = table.Column<int>(nullable: true),
+                    CourseId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProgramCourses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProgramCourses_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalSchema: "Common",
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ProgramCourses_Programs_ProgramId",
+                        column: x => x.ProgramId,
+                        principalSchema: "CareerTech",
+                        principalTable: "Programs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProgramCourses_CourseId",
+                schema: "CareerTech",
+                table: "ProgramCourses",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProgramCourses_ProgramId",
+                schema: "CareerTech",
+                table: "ProgramCourses",
+                column: "ProgramId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProgramCourses",
+                schema: "CareerTech");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SubjectAreaId",
+                schema: "Common",
+                table: "Courses",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
---
+++ CourseDemo.Data/Migrations/20200318195559_program-assignments-update.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseDemo.Data.Migrations
+{
+    public partial class programassignmentsupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProgramCourses_Courses_CourseId",
+                schema: "CareerTech",
+                table: "ProgramCourses");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BeginYear",
+                schema: "CareerTech",
+                table: "ProgramCourses",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EndYear",
+                schema: "CareerTech",
+                table: "ProgramCourses",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProgramCourses_Courses_CourseId",
+                schema: "CareerTech",
+                table: "ProgramCourses",
+                column: "CourseId",
+                principalSchema: "Common",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProgramCourses_Courses_CourseId",
+                schema: "CareerTech",
+                table: "ProgramCourses");
+
+            migrationBuilder.DropColumn(
+                name: "BeginYear",
+                schema: "CareerTech",
+                table: "ProgramCourses");
+
+            migrationBuilder.DropColumn(
+                name: "EndYear",
+                schema: "CareerTech",
+                table: "ProgramCourses");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProgramCourses_Courses_CourseId",
+                schema: "CareerTech",
+                table: "ProgramCourses",
+                column: "CourseId",
+                principalSchema: "Common",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
