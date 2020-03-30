04bc8cb8a3fdeb9ed35532d88c330ef07fde287d
Mark <mark.lawrence@c2cschools.com>
2020-03-30 11:09:01 -0500

    credentials migration

---
+++ CourseDemo.Data/Migrations/20200330154954_credentials.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseDemo.Data.Migrations
+{
+    public partial class credentials : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Credentials",
+                schema: "CareerTech",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CredentialCode = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    BeginYear = table.Column<int>(nullable: true),
+                    EndYear = table.Column<int>(nullable: true),
+                    IsReimbursable = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Credentials", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Credentials",
+                schema: "CareerTech");
+        }
+    }
+}
